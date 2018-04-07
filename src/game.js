380bf82016f2349a33119c70d597a9034a4aa754
dgk2 <dgk2@pc3-029-l.cs.st-andrews.ac.uk>
2018-04-07 17:21:58 +0100

    Choice of solidity

--- src/game.js
+++ src/game.js
@@ -10,10 +10,10 @@ var Game = function () {
   //the character the player is playing as
   let character;
 
-  grass = new GridCell("green", "textures/grass.png");
-  water = new GridCell("green", "textures/water.png");
-  tower = new GridCell("green", "textures/tower.png");
-  tree = new GridCell("#68a2ff", "textures/tree.png");
+  grass = new GridCell("green", "textures/grass.png", true);
+  water = new GridCell("green", "textures/water.png", false);
+  tower = new GridCell("green", "textures/tower.png", true);
+  tree = new GridCell("#68a2ff", "textures/tree.png", true);
 
   let level = {
     background: "#68a2ff",
