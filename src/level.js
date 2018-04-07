0c6bdf7bdd15e59d82ef0020bbd8e6893b17d058
dgk2 <dgk2@pc3-029-l.cs.st-andrews.ac.uk>
2018-04-07 19:31:11 +0100

    Semi-fixed

--- src/level.js
+++ src/level.js
@@ -27,7 +27,7 @@ function isObstructed(level, x, y) {
     let tile = getTile(level, x, y);
     if (tile === undefined) return true;
     if (tile === null) return false;
-    return tile.solid;
+    return tile.isSolid;
 }
 
 function getTile(level,x , y) {
@@ -35,9 +35,9 @@ function getTile(level,x , y) {
     let gridY = Math.floor(y / GRID_SIZE);
 
     if (gridX >= 0 && gridX < level.grid.length && gridY >= 0 && gridY < level.grid[gridX].length) {
-        return level.grid[gridX][gridY] ? level.grid[gridX][gridY].isSolid : false;
+        return level.grid[gridX][gridY];
     } else {
-        return true;
+        return undefined;
     }
 }
 
